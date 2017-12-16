9c29b3e9e3691a29e18a8820e63cd85fc2ce28b1
Paul Smith <paulcsmith0218@gmail.com>
2017-12-15 23:00:36 -0500

    Add more test cases

--- spec/lucky/protect_from_forgery_spec.cr
+++ spec/lucky/protect_from_forgery_spec.cr
@@ -5,10 +5,16 @@ private class ProtectAction < Lucky::Action
 end
 
 describe Lucky::ProtectFromForgery do
-  it "continues if the token is correct" do
+  it "continues if the token in the parameter is correct" do
+  end
+
+  it "continues if the token in the header is correct" do
   end
 
   it "halts with 403 if the token is incorrect" do
+  end
+
+  it "halts with 403 if the token is missing" do
   end
 
   it "lets allowed HTTP methods through without a token" do
