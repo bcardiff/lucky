35a3225aedb5b472050fb8c194773036886d468c
Paul Smith <paulcsmith0218@gmail.com>
2017-12-21 18:30:51 -0500

    Getting closer

--- spec/lucky/protect_from_forgery_spec.cr
+++ spec/lucky/protect_from_forgery_spec.cr
@@ -1,20 +1,59 @@
 require "../spec_helper"
 
-private class ProtectAction < Lucky::Action
+include ContextHelper
+
+class ProtectedAction::Index < Lucky::Action
   include Lucky::ProtectFromForgery
+
+  action { render_text "Passed" }
 end
 
 describe Lucky::ProtectFromForgery do
   it "continues if the token in the parameter is correct" do
+    context = build_context
+    context.session["X-CSRF-TOKEN"] = "my_token"
+    params = {"_csrf" => "my_token"}
+
+    response = ProtectedAction::Index.new(context, params).call
+
+    response.status.should eq(200)
+    response.body.should eq("Passed")
   end
 
   it "continues if the token in the header is correct" do
+    context = build_context
+    context.session["X-CSRF-TOKEN"] = "my_token"
+    context.request.headers["X-CSRF-TOKEN"] = "my_token"
+
+    response = ProtectedAction::Index.new(context, params).call
+
+    response.status.should eq(200)
+    response.body.should eq("Passed")
   end
 
-  it "halts with 403 if the token is incorrect" do
+  it "halts with 403 if the header token is incorrect" do
+    context = build_context
+    context.session["X-CSRF-TOKEN"] = "my_token"
+    context.request.headers["X-CSRF-TOKEN"] = "incorrect"
+
+    response = ProtectedAction::Index.new(context, params).call
+
+    response.status.should eq(403)
+    response.body.should eq("")
   end
 
-  it "halts with 403 if the token is missing" do
+  pending "halts with 403 if the param token is incorrect" do
+    context = build_context
+    context.session["X-CSRF-TOKEN"] = "my_token"
+    context.request.headers["X-CSRF-TOKEN"] = "my_token"
+
+    response = ProtectedAction::Index.new(context, params).call
+
+    response.status.should eq(200)
+    response.body.should eq("Passed")
+  end
+
+  it "halts with 403 if there is no token" do
   end
 
   it "lets allowed HTTP methods through without a token" do
--- src/lucky/protect_from_forgery.cr
+++ src/lucky/protect_from_forgery.cr
@@ -4,5 +4,26 @@ module Lucky::ProtectFromForgery
   end
 
   private def protect_from_forgery
+    if valid_csrf_token?
+      continue
+    else
+      forbid_access_because_of_missing_token
+    end
+  end
+
+  private def valid_csrf_token?
+    session_token == user_provided_token
+  end
+
+  private def session_token
+    session["X-CSRF-TOKEN"]
+  end
+
+  private def user_provided_token
+    params.get("_csrf") || headers["X-CSRF-TOKEN"]
+  end
+
+  def forbid_access_because_of_missing_token
+    head Lucky::Action::Status::Forbidden
   end
 end
